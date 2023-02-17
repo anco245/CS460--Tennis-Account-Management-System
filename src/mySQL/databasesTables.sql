675217ff6f31e1ca0a571a947642798b505ccc72
John Cifone <john.c6485@gmail.com>
2023-02-17 15:45:25 -0500

    added on cascade function

--- src/mySQL/databasesTables.sql
+++ src/mySQL/databasesTables.sql
@@ -5,20 +5,21 @@ USE courtsystem;
 CREATE TABLE directory (
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
-    age VARCHAR(5) NOT NULL,
+	age VARCHAR(5) NOT NULL,
     address VARCHAR(60) NOT NULL,
     phone VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL PRIMARY KEY,
+	username VARCHAR(50) NOT NULL PRIMARY KEY,
     pword VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE reservation (
-    courtNum int NOT NULL PRIMARY KEY,
+	courtNum int NOT NULL PRIMARY KEY,
     username VARCHAR(50),
     resTime datetime,
     isRes bool NOT NULL,
     FOREIGN KEY (username) REFERENCES directory(username) 
+		ON DELETE CASCADE
 );
 
 INSERT INTO reservation (courtNum, isRes)
