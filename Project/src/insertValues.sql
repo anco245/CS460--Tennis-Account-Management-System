833a381c3b78fd80f4feddc9c314d2989761c24f
John Cifone <john.c6485@gmail.com>
2023-02-28 02:16:42 -0500

    changed names of files

---
+++ Project/src/insertValues.sql
@@ -0,0 +1,2 @@
+INSERT INTO directory (firstName, lastName, age, address, phone, email, username, pword) 
+VALUES ("John", "Cifone", "21", "1 Main Street", "4353234432", "john@gmail.com", "jcifone", "pass")
