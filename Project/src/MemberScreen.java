7d3d1ba50298c9048e2947a8e1925ec3a5ee0f6f
EvanCopeland <78445857+EvanCopeland@users.noreply.github.com>
2023-03-10 16:26:54 -0500

    Update MemberScreen.java

--- Project/src/MemberScreen.java
+++ Project/src/MemberScreen.java
@@ -1,3 +1,20 @@
 public class MemberScreen {
+    public void makeReservation(){
     
+    }
+    public void cancelReservation(){
+
+    }
+    public void cancelMembership(){
+
+    }
+    public void addGuest(){
+
+    }
+    public void financialInformation(){
+
+    }
+    public void viewClubHours(){
+
+    }
 }
