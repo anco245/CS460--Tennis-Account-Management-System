7f4c2ce8cb67dda4bad68fb3c510ae09714b4e0c
John Cifone <john.c6485@gmail.com>
2023-03-22 14:56:51 -0400

    deleted project

--- Project/src/insertValues.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO directory (firstName, lastName, age, address, phone, email, username, pword, verified, shown) 
-VALUES ("john", "doe", "21", "1 Main Street", "4353234432", "john@tennis.com", "jdoe", "pass", true, false);
-
-INSERT INTO directory (firstName, lastName, age, address, phone, email, username, pword, verified, shown) 
-VALUES ("jane", "smith", "67", "123 Main Street", "758495847", "jane@gmail.com", "jsmith", "pass", false, true)
