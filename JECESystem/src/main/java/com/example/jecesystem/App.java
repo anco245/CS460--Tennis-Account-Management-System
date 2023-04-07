bb320d1a0a118eb2cf61e31afee567e6464e8621
Chris Johnson <jazztrombone007@gmail.com>
2023-04-06 20:14:58 -0400

    fixed some fxmls

--- JECESystem/src/main/java/com/example/jecesystem/App.java
+++ JECESystem/src/main/java/com/example/jecesystem/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
   @Override
   public void start(Stage stage) throws IOException {
     //login.fxml is the first screen we see
-    scene = new Scene(loadFXML("login"), 1067, 600);
+    scene = new Scene(loadFXML("login"), 1280, 720);
     stage.setScene(scene);
     stage.show();
   }
