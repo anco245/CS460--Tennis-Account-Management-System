ece511b32194f8349c566dc246be4ea17c1d9e45
John Cifone <john.c6485@gmail.com>
2023-04-10 13:21:16 -0400

    changed file

--- SQL/insertValues.sql
+++ SQL/insertValues.sql
@@ -2,7 +2,7 @@ INSERT INTO directory (firstName, lastName, age, address, phone, email, username
 VALUES ("John", "Doe", 21, "1 Main Street", "4353234432", "john@tennis.com", "jdoe", "pass", true, false, false, false, 0, 0, true, true);
 
 INSERT INTO directory (firstName, lastName, age, address, phone, email, username, pword, verified, shown, late, penalized, owe, guests, keepAccount, keepConfirm) 
-VALUES ("Jane", "Smith", 67, "123 Main Street", "9758495847", "jane@gmail.com", "jsmith", "pass", false, true, true, false, 1000, 0, false, false);
+VALUES ("Jane", "Smith", 67, "123 Main Street", "9758495847", "jane@gmail.com", "jsmith", "pass", false, true, true, false, 1000, 0, true, true);
 
 INSERT INTO directory (firstName, lastName, age, address, phone, email, username, pword, verified, shown, late, penalized, owe, guests, keepAccount, keepConfirm) 
 VALUES ("Adam", "Fincher", 33, "123 Second Street", "7598495847", "admin9999@admin.com", "admin", "pass", true, false, false, false, 0, 0, true, true);
