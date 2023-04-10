21787eb96e3af06c7e7a04325cc1769c35832e65
John Cifone <john.c6485@gmail.com>
2023-04-10 16:24:02 -0400

    make res

--- JECESystem/src/main/java/com/example/jecesystem/MakeResController.java
+++ JECESystem/src/main/java/com/example/jecesystem/MakeResController.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 
@@ -17,12 +18,17 @@ import java.util.ResourceBundle;
 public class MakeResController implements Initializable {
 
   @FXML
-  private ChoiceBox<?> courtNum;
+  private ChoiceBox<Integer> courtNum;
+
+  @FXML
+  private ChoiceBox<String> dayOfWeek;
 
   @FXML
   private CheckBox nineAM;
 
-  ObservableList list = FXCollections.observableArrayList();
+  ObservableList day = FXCollections.observableArrayList();
+  ObservableList court = FXCollections.observableArrayList();
+  //ObservableList = FXCollections.observableArrayList();
 
   @Override
   public void initialize(URL url, ResourceBundle rb)
@@ -31,7 +37,7 @@ public class MakeResController implements Initializable {
   }
 
   private void loadData() {
-    list.removeAll(list);
+    court.removeAll(court);
 
     int a = 1;
     int b = 2;
@@ -46,26 +52,69 @@ public class MakeResController implements Initializable {
     int k = 11;
     int l = 12;
 
-    list.addAll(a, b, c, d, e, f, g, h, i, j, k, l);
+    court.addAll(a, b, c, d, e, f, g, h, i, j, k, l);
+    courtNum.getItems().addAll(court);
 
-    //courtNum.getItems().addAll(list);
+    day.removeAll(day);
+
+    String monday = "Monday";
+    String tuesday = "Tuesday";
+    String wednesday = "Wednesday";
+    String thursday = "Thursday";
+    String friday = "Friday";
+    String saturday = "Saturday";
+    String sunday = "Sunday";
+
+    day.addAll(monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+    dayOfWeek.getItems().addAll(day);
   }
 
   @FXML
   void submitreservation(ActionEvent event) {
 
-    //System.out.println(num);
-    /*
-    if(Database.checkRes(num))
-    {
-      System.out.println("Court is already reserved");
-      //show this in fxml
-    } else if (Database.inReservation(name)) {
-      System.out.println("You have already reserved a court");
-      //show this in fxml
+    Alert con = new Alert(Alert.AlertType.CONFIRMATION);
+    Alert error = new Alert(Alert.AlertType.ERROR);
+
+    LocalDateTime now = LocalDateTime.now();
+
+    int givenCourt = courtNum.getValue();
+    String givenDay = dayOfWeek.getValue();
+    String timeOfRes = "";
+
+    if (Database.checkRes(givenCourt)) {
+      error.setTitle("Error");
+      error.setContentText("This court has already been reserved.\nTry Again.");
+      error.showAndWait();
+    } else if (Database.inReservation(Database.memberUser)) {
+      error.setTitle("Error");
+      error.setContentText("You've already reserved a court for today.\nTry Again.");
+      error.showAndWait();
     } else {
-      //Database.makeRes(num, name, time);
+      if(nineam.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (nine30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (ten.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (ten30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (eleven.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (eleven30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (noon.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (noon30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (one.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (one30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (two.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (two30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (three.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (three30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (four.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (four30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (five.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (five30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (six.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (six30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (seven.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (seven30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (eight.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (eight30.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
+      else if (ninepm.isSelected()){timeOfRes = "20120618 9:00:00 AM"; Database.makeRes(givenCourt, givenDay, timeOfRes);}
     }
-    */
   }
 }
