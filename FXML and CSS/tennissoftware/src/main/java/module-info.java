63e1d995b2c057a8104ee470dc5793f04917f235
John Cifone <john.c6485@gmail.com>
2023-04-23 14:08:39 -0400

    deleted unessessary files

--- FXML and CSS/tennissoftware/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example to javafx.fxml;
-    exports com.example;
-}
